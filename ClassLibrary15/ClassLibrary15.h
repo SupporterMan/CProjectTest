6394f8310b5e61ccb921fa0194fdbc3bb9276000
v-weiwca <v-weiwca@WXV-JAKE-W11>
2017-08-03 15:26:21 +0800

    Add project files.

---
+++ ClassLibrary15/ClassLibrary15.h
@@ -0,0 +1,13 @@
+// ClassLibrary15.h
+
+#pragma once
+
+using namespace System;
+
+namespace ClassLibrary15 {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
